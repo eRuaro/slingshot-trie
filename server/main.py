bbe981b4ffea639428f5a95225e862b54d1a9814
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-07-12 18:29:01 +0800

    started on server

---
+++ server/main.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from fastapi import FastAPI
+import trie.trie as trie
+
+app = FastAPI()
+trie = trie.Trie()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+@app.put("/addWord/{word}")
+async def add_word(word: str):
+    trie.addWord(word)                   
+    return {"status": "ok"}
+
+@app.get("/suggestions/{prefix}")
+async def get_suggestions(prefix : str, suggestion_nums=5):
+    suggestions = trie.suggest(prefix)
+    return {"suggestions": suggestions[:suggestion_nums]}

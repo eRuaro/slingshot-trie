6748441cb1715863ff6ab48aee6da7da60e720ce
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-07-13 18:40:32 +0800

    added methods for testing suggest functionality

--- server/tests/test_main.py
+++ server/tests/test_main.py
@@ -54,6 +54,7 @@ def test_get_method_searching_word_in_trie():
     response = client.get("/search/hello")
     assert response.status_code == 200
     assert response.json() == {"status": "hello found in Trie!"}
+
 def test_get_method_searching_word_not_in_trie():
     """
     Tests searching a word not in the trie
@@ -62,3 +63,29 @@ def test_get_method_searching_word_not_in_trie():
     assert response.status_code == 200       
     #slingshot was deleted in an earlier test so it should not be in the trie anymore
     assert response.json() == {"status": "slingshot not found in Trie!"}
+
+def test_get_method_suggestion():
+    """
+    Tests retrieving a list of suggestions in the Trie
+    """
+    client.post("/add-word/real madrid")
+    client.post("/add-word/real bacon")
+    client.post("/add-word/fake bacon")
+    client.post("/add-word/real madrid")
+    response = client.get("/suggestions/real")
+    assert response.status_code == 200
+    assert response.json() == ["real madrid", "real bacon"]
+
+def test_get_method_suggestion_modified():
+    """
+    Tests retrieving a list of 3 suggestions in the Trie
+    """
+    client.post("/add-word/sli")
+    client.post("/add-word/slingboy")
+    client.post("/add-word/slinggirl")
+    client.post("/add-word/slinggirl")
+    client.post("/add-word/slingshot")
+    response = client.get("/suggestions/sli?suggestion_nums=3")
+    assert response.status_code == 200
+    assert response.json() == ["slinggirl", "slingshot is cool","sli"]
+

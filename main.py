150762236e1eb5938ec9254c1bf99466400a5bab
eRuaro <erickson_ruaroii@dlsu.edu.ph>
2021-07-14 08:14:45 +0800

    finished CLI app

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = typer.Typer()
 url = "https://w0j5na.deta.dev"
 
 @app.command()
-def add_word(word : str):
+def add_word(word : str = typer.Argument(..., help="Adds a word into the trie")):
     """
     Adds word to the trie API
     """
@@ -16,7 +16,7 @@ def add_word(word : str):
     
 
 @app.command()
-def search_word(word : str):
+def search_word(word : str = typer.Argument(..., help="Searches the trie if the word exists")):
     """
     Searches if a word is in the Trie API server
     """
@@ -25,7 +25,7 @@ def search_word(word : str):
     typer.echo(response.json()["status"])
     
 @app.command()
-def delete_word(word : str):
+def delete_word(word : str = typer.Argument(..., help="The word to be deleted from the Trie")):
     """
     Deletes a word in the Trie API server
     """
@@ -34,32 +34,32 @@ def delete_word(word : str):
     typer.echo(response.json()["status"])
 
 @app.command()
-def suggestions(prefix : str = typer.Argument(...), suggestion_nums : int = typer.Argument(5)):
+def suggestions(prefix : str = typer.Argument(...), n : int = typer.Option(5, help="Number of suggestions to display")):
     """
     Returns a list of suggestions for a given prefix in the Trie API. Defaults to giving the top 5    
     """       
-    response_url = url + "/suggestions/" + prefix + "?suggestion_nums=" + str(suggestion_nums)       
+    response_url = url + "/suggestions/" + prefix + "?suggestion_nums=" + str(n)       
     response = requests.get(response_url)      
     for i in range(len(response.json())):
         typer.echo(response.json()[i])
         
 @app.command()
-def display(display_nums : int = typer.Argument(5), display_all : bool = typer.Argument(False)):
+def display(n : int = typer.Option(5, help="Default the top n words in the trie"), all : bool = typer.Option(False, help="Displays all the words in the trie")):
     """
     Displays the words in the Trie API server. Defaults to displaying the top 5 words.
     """
     response_url = url + "/display-trie"            
     response = requests.get(response_url)   
     typer.echo(response.json())
-    if display_all:
+    if all:
         for i in range(len(response.json())):        
             typer.echo(response.json()[i])
     else:
-        if display_nums > len(response.json()):
+        if n > len(response.json()):
             for i in range(len(response.json())):
                 typer.echo(response.json()[i])
         else:
-            for i in range(display_nums):
+            for i in range(n):
                 typer.echo(response.json()[i])
 
 if __name__ == "__main__":
